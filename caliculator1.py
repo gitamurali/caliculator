3cc0f86efbc5f9926c69bdd37481229fbc4e50d6
gitamurali <106994352+gitamurali@users.noreply.github.com>
2022-06-16 15:26:20 -0400

    here we created two functions add and sub to perform operation and return the vale.

--- caliculator1.py
+++ caliculator1.py
@@ -1,9 +1,23 @@
-ef caliculator():
+
+
+def add(n1,n2):
+    return n1 + n2
+def sub(n1,n2):
+    return n1 - n2
+
+
+
+def caliculator():
   #operations = {"+": add(x, y), "-": sub(x, y), "*": multi(x, y), "/": div(x, y)}
 
   x = int(input("What is you first number: "))
   op=input("what operation you want to perform '+,-,*,/' : ")
-  y = int(input("Enter your second number: "))
+  y = int(input("Enter your secdef add(n1,n2):
+  return n1 + n2
+
+def sub(n1,n2):
+  return n1 - n2
+ond number: "))
   operations = {"+": add(x, y), "-": sub(x, y), "*": multi(x, y), "/": div(x, y)}
 
   result= operations[op]
