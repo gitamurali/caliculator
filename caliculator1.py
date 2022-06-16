29b8a835a7bbc953cebe9826d06d59e470cba0e8
gitamurali <106994352+gitamurali@users.noreply.github.com>
2022-06-16 16:37:25 -0400

    Added mod operation to the existing caliculator.

--- caliculator1.py
+++ caliculator1.py
@@ -6,10 +6,13 @@ def sub(n1,n2):
     return n1 - n2
 
 def multi(n1,n2):
-  return n1 * n2
+    return n1 * n2
 
 def div(n1,n2):
-  return n1 / n2
+    return n1 / n2
+
+def mod(n1,n2):
+    return n1 % n2
 def again():
   cal = input("Do you want to continue y or n : ")
   if cal=="y":
@@ -21,9 +24,9 @@ def caliculator():
   #operations = {"+": add(x, y), "-": sub(x, y), "*": multi(x, y), "/": div(x, y)}
 
   x = int(input("What is you first number: "))
-  op=input("what operation you want to perform '+,-,*,/' : ")
-  y = int(input("Enter your secdef add(n1,n2):
-  operations = {"+": add(x, y), "-": sub(x, y), "*": multi(x, y), "/": div(x, y)}
+  op=input("what operation you want to perform '+,-,*,/,%' : ")
+  y = int(input("Enter your second number: ")
+  operations = {"+": add(x, y), "-": sub(x, y), "*": multi(x, y), "/": div(x, y), "%": mod(x,y)}
 
   result= operations[op]
   print(result)
