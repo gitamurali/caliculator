9cf2fa66b1b4ecc95bf7b847620dec00fde29995
gitamurali <106994352+gitamurali@users.noreply.github.com>
2022-06-16 16:02:32 -0400

    deleted unwanted code.

--- caliculator1.py
+++ caliculator1.py
@@ -23,11 +23,6 @@ def caliculator():
   x = int(input("What is you first number: "))
   op=input("what operation you want to perform '+,-,*,/' : ")
   y = int(input("Enter your secdef add(n1,n2):
-  return n1 + n2
-
-def sub(n1,n2):
-  return n1 - n2
-ond number: "))
   operations = {"+": add(x, y), "-": sub(x, y), "*": multi(x, y), "/": div(x, y)}
 
   result= operations[op]
