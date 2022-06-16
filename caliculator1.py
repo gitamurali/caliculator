d737f8b7467e80302210ed2e48f18e55ecb1b252
gitamurali <106994352+gitamurali@users.noreply.github.com>
2022-06-16 15:45:13 -0400

    If we want to perform caliculation again here we are using again() function

--- caliculator1.py
+++ caliculator1.py
@@ -10,7 +10,12 @@ def multi(n1,n2):
 
 def div(n1,n2):
   return n1 / n2
-
+def again():
+  cal = input("Do you want to continue y or n : ")
+  if cal=="y":
+      caliculator()
+  elif cal=="n":
+      print("good bye!")
 
 def caliculator():
   #operations = {"+": add(x, y), "-": sub(x, y), "*": multi(x, y), "/": div(x, y)}
